3e922777a81be16202bd2f0fa0d8706965a3955e
satchidipu174 <46513294+satchidipu174@users.noreply.github.com>
2022-02-06 22:02:14 +0530

    Delete AWS_ec2_meta.sh

--- AWS_ec2_meta.sh
+++
@@ -1,1 +0,0 @@
- aws ec2 describe-instances --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,PrivateIP:PrivateIpAddress,InstanceID:InstanceId,ImageID:ImageId,KeyName:KeyName,InstanceType:InstanceType,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table
